faa609babe5f8d4d0f9a7b053af5ac2301310740
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-08-14 22:44:17 +0800

    basic client

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,70 @@
 import ws from "websocket"
 import { ExchangeReq } from "./cluster-rpc_pb"
+import { EventEmitter } from "events"
 
-const sock = new ws.client()
-sock.on("connect", conn => {
-    conn.on("message", data => {
-        const test = ""
+class WebSocket {
+    private readonly conn: ws.connection
+    private readonly emitter: EventEmitter
+    constructor(conn: ws.connection, emitter: EventEmitter) {
+        this.conn = conn
+        this.emitter = emitter
+    }
+    close() {
+        this.conn.close()
+    }
+    on(event: "message", cb: (data: string)=> void) {
+        this.emitter.on(event, cb)
+    }
+    off(event: "message", cb: (data: string)=> void) {
+        this.emitter.off(event, cb)
+    }
+    emit(event: "emit:room" | "emit:socket" | "room:join", id: string, data: string) {
+        this.emitter.emit(event, id, data)
+    }
+}
+
+export default function Create (host: string): Promise<WebSocket>{
+    return new Promise<WebSocket>(res=> {
+        const emitter = new EventEmitter()
+        const sock = new ws.client()
+        sock.on("connect", conn => {
+            conn.on("message", data => {
+                const req = ExchangeReq.deserializeBinary((data as any).binaryData)
+                emitter.emit("message", Buffer.from(req.message).toString())
+            })
+            emitter.on("room:join", (room, data) => {
+                const req = new ExchangeReq()
+                req.event = "room:join"
+                req.to = room
+                conn.sendBytes(Buffer.from(req.serializeBinary()))
+            })
+            emitter.on("emit:room", (room, data) => {
+                const req = new ExchangeReq()
+                req.event = "emit:room"
+                req.to = room
+                req.message = Buffer.from(data)
+                conn.sendBytes(Buffer.from(req.serializeBinary()))
+            })
+            emitter.on("emit:socket", (socket, data) => {
+                const req = new ExchangeReq()
+                req.event = "emit:socket"
+                req.to = socket
+                req.message = Buffer.from(data)
+                conn.sendBytes(Buffer.from(req.serializeBinary()))
+            })
+            return res(new WebSocket(conn, emitter))
+        })
+        sock.connect(host);
     })
-    const req = new ExchangeReq()
-    req.event = "room:join"
-    req.to = "test"
-    conn.sendBytes(Buffer.from(req.serializeBinary()))
-})
-sock.connect("ws://127.0.0.1:7000/comms")
+}
+
+
+async function main() {
+    const conn = await Create("ws://127.0.0.1:8000/comms")
+    conn.emit("room:join", "test", "")
+    setInterval(()=> {
+        conn.emit("emit:room", "test", "ok")
+    }, 5000)
+}
+
+main()
